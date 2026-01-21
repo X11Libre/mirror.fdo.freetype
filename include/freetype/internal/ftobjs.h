cb532fcbc18a9c4b29e574241e878c423c0af540
Alexei Podtelezhnikov <apodtele@gmail.com>
2026-01-20 23:09:39 -0500

    [smooth] Implement direct LCD filtering.
    Applying LCD filter to spans rather than the entire image improves
    performance of ClearType-like rendering by about 40% at 32 ppem and
    much more at larger sizes.
    
    This also removes support of legacy filter and per-face weights.
    Both of these features are misleading and should not be used.

--- include/freetype/internal/ftobjs.h
+++ include/freetype/internal/ftobjs.h
@@ -297,19 +297,6 @@ FT_BEGIN_HEADER
   find_unicode_charmap( FT_Face  face );
 
 
-#ifdef FT_CONFIG_OPTION_SUBPIXEL_RENDERING
-
-  typedef void  (*FT_Bitmap_LcdFilterFunc)( FT_Bitmap*      bitmap,
-                                            FT_Byte*        weights );
-
-
-  /* This is the default LCD filter, an in-place, 5-tap FIR filter. */
-  FT_BASE( void )
-  ft_lcd_filter_fir( FT_Bitmap*           bitmap,
-                     FT_LcdFiveTapFilter  weights );
-
-#endif /* FT_CONFIG_OPTION_SUBPIXEL_RENDERING */
-
   /**************************************************************************
    *
    * @struct:
@@ -365,11 +352,6 @@ FT_BEGIN_HEADER
    *     Value~0 means to use the font's value.  Value~-1 means to use the
    *     CFF driver's default.
    *
-   *   lcd_weights ::
-   *   lcd_filter_func ::
-   *     These fields specify the LCD filtering weights and callback function
-   *     for ClearType-style subpixel rendering.
-   *
    *   refcount ::
    *     A counter initialized to~1 at the time an @FT_Face structure is
    *     created.  @FT_Reference_Face increments this counter, and
@@ -390,11 +372,6 @@ FT_BEGIN_HEADER
 
     FT_Char              no_stem_darkening;
     FT_Int32             random_seed;
-
-#ifdef FT_CONFIG_OPTION_SUBPIXEL_RENDERING
-    FT_LcdFiveTapFilter      lcd_weights;      /* filter weights, if any */
-    FT_Bitmap_LcdFilterFunc  lcd_filter_func;  /* filtering callback     */
-#endif
 
     FT_Int  refcount;
 
@@ -889,10 +866,6 @@ FT_BEGIN_HEADER
    *   lcd_weights ::
    *     The LCD filter weights for ClearType-style subpixel rendering.
    *
-   *   lcd_filter_func ::
-   *     The LCD filtering callback function for for ClearType-style subpixel
-   *     rendering.
-   *
    *   lcd_geometry ::
    *     This array specifies LCD subpixel geometry and controls Harmony LCD
    *     rendering technique, alternative to ClearType.
@@ -926,7 +899,6 @@ FT_BEGIN_HEADER
 
 #ifdef FT_CONFIG_OPTION_SUBPIXEL_RENDERING
     FT_LcdFiveTapFilter      lcd_weights;      /* filter weights, if any */
-    FT_Bitmap_LcdFilterFunc  lcd_filter_func;  /* filtering callback     */
 #else
     FT_Vector                lcd_geometry[3];  /* RGB subpixel positions */
 #endif
